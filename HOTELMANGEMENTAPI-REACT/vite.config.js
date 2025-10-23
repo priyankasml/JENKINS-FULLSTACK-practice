91cfc4679f45c104a135fdfcd06d387cb1f718e8
unknown <priyankasml54@gmail.com>
2025-10-23 14:17:24 +0530

    commited;

--- HOTELMANGEMENTAPI-REACT/vite.config.js
+++ HOTELMANGEMENTAPI-REACT/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',   // <-- Use relative paths for Tomcat deployment
+  base: '/hotelmanagement/',   // <-- Use relative paths for Tomcat deployment
   plugins: [react()],
 })
